5c63c639ffebe11f06688c9ec5ef692b0fcf40d6
Michel Pouleijn <m.pouleijn@gynzy.com>
2017-02-21 21:06:52 +0100

    First test

--- jsonapi-base.gemspec
+++ jsonapi-base.gemspec
@@ -4,23 +4,24 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'jsonapi/base/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "jsonapi-base"
+  spec.name          = 'jsonapi-base'
   spec.version       = Jsonapi::Base::VERSION
-  spec.authors       = ["Michel Pouleijn"]
-  spec.email         = ["m.pouleijn@gynzy.com"]
+  spec.authors       = ['Michel Pouleijn']
+  spec.email         = ['michel@pouleijn.nl']
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
-  spec.license       = "MIT"
+  spec.summary       = %q{JSONAPI base inheritance for rails}
+  spec.description   = %q{This gem create's a base setup according to the JSONAPI spec}
+  spec.homepage      = 'https://github.com/mpouleijn/jsonapi-base'
+  spec.license       = 'MIT'
 
+  # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
+  # to allow pushing to a single host or delete this section to allow pushing to any host.
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
   if spec.respond_to?(:metadata)
     spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
   else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
+    raise 'RubyGems 2.0 or newer is required to protect against public gem pushes.'
   end
 
   spec.files         = `git ls-files -z`.split("\x0").reject do |f|
@@ -33,4 +34,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.14"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+
+  spec.add_dependency 'jsonapi-serializers'
+  spec.add_dependency 'pundit'
 end
--- lib/jsonapi/base.rb
+++ lib/jsonapi/base.rb
@@ -1,7 +1,169 @@
 require "jsonapi/base/version"
 
-module Jsonapi
+module JSONAPI
   module Base
-    # Your code goes here...
+    extend ActiveSupport::Concern
+
+    included do
+      if respond_to?(:hide_action)
+        hide_action :alias_parsing
+        hide_action :attributes
+        hide_action :before_create
+        hide_action :before_destroy
+        hide_action :before_update
+        hide_action :create_failure
+        hide_action :create_success
+        hide_action :destroy_failure
+        hide_action :destroy_success
+        hide_action :update_failure
+        hide_action :update_success
+        hide_action :model
+        hide_action :parameter_aliases
+      end
+
+      if respond_to?(:before_action)
+        before_action :alias_parsing
+        before_action :before_create, only: [:create]
+        before_action :before_update, only: [:update]
+        before_action :before_destroy, only: [:destroy]
+      end
+
+    end
+
+    def show
+      resource = JSONAPI::FetchData::Resource.new(model).find(params)
+      authorize resource
+
+      include_param = params.fetch(:include, nil)
+      render_model resource, include: include_param.nil? ? nil : include_param.scan(/[-\w]+/)
+    end
+
+    def index
+      resources = JSONAPI::FetchData::Collection.new(policy_scope(model)).find(params).all
+
+      serialize_attr = {
+        include: params.fetch(:include, '').scan(/[-\w]+/),
+        fields: params.fetch(:fields, {}).deep_symbolize_keys,
+      }
+
+      render_models resources, serialize_attr
+    end
+
+    def create
+      resource = model.new(attributes)
+      authorize resource
+
+      if resource.valid?
+        resource.save
+        create_success(resource)
+        render_model resource, status: 201
+      else
+        create_failure(resource)
+        render_errors({ errors: resource.errors })
+      end
+    end
+
+    def update
+      resource = model.find(params[:id].to_i)
+      authorize resource
+
+      if resource && resource.update_attributes(attributes)
+        update_success(resource)
+        render_model resource, status: 201
+      else
+        update_failure(resource)
+        render_errors({ errors: resource.errors })
+      end
+    end
+
+    def destroy
+      resource = model.find_by_id(params[:id])
+
+      if resource
+        authorize resource
+        resource.destroy
+        destroy_success(resource)
+        render_model resource, status: 202
+      else
+        skip_authorization
+        destroy_failure(resource)
+        render_404('404')
+      end
+    end
+
+    # This error will be given when there is no correct route found
+    def action_not_found
+      skip_authorization
+      render_501
+    end
+
+    # private
+
+    # overwrite to allow aliases in attributes
+    def attributes
+      return @attributes if defined?(@attributes)
+      hash = params.require(:data).require(:attributes).permit(*policy(model).permitted_attributes)
+      @attributes = HashKeys.deep_symbolize_and_underscore(hash)
+
+      parameter_aliases.each do |k,v|
+        @attributes[k] = @attributes.delete(v) if @attributes.has_key?(v)
+      end if parameter_aliases.any?
+
+      @attributes
+    end
+
+    # Create methods
+    def before_create
+    end
+
+    def create_failure(resource)
+    end
+
+    def create_success(resource)
+    end
+
+    # Update methods
+    def before_update
+    end
+
+    def update_failure(resource)
+    end
+
+    def update_success(resource)
+    end
+
+    # Destroy methods
+    def before_destroy
+    end
+
+    def destroy_failure(resource)
+    end
+
+    def destroy_success(resource)
+    end
+
+    def alias_parsing
+      if parameter_aliases.any?
+        filters = params.fetch(:filter, {})
+        sort = params.fetch(:sort, nil)
+
+        params_aliases = {}
+        parameter_aliases.each do |k,v|
+          filters[k] = filters.delete(v) if filters.has_key?(v)
+          sort.gsub!(v.to_s, k.to_s) if sort && (sort.length > 0)
+        end
+        params_aliases[:filter] = filters if filters.any?
+        params_aliases[:sort] = sort unless sort.blank?
+        params.merge!(params_aliases)
+      end
+    end
+
+    def model
+      controller_name.classify.constantize
+    end
+
+    def parameter_aliases
+      {}
+    end
   end
 end
